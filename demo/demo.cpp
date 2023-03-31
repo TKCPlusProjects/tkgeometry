54dea6b5fb43f51606a70fbf1ada70f7eca51df9
ZhengXianda <xianda.zheng@jiamiantech.com>
2023-03-31 18:34:00 +0800

    优化demo代码，封装Voronoi图绘制函数

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -4,15 +4,79 @@
 
 #include <iostream>
 
-int count = 200;
-int margin = 50;
-int width, height = 0;
-
 using namespace std;
 using namespace tkht;
 
+int width, height = 0;
+
+int margin = 50;
+
 void vertex2d (GLdouble x, GLdouble y) {
   glVertex2d((x + margin)/width * 2.0f - 1.0f, (y + margin)/height * 2.0f - 1.0f);
+}
+
+void drawVoronoi (GLFWwindow* window, int count) {
+  int _width;
+  int _height;
+  glfwGetWindowSize(window, &_width, &_height);
+  
+  if (width == _width && height == _height) {
+    
+  } else {
+    width = _width;
+    height = _height;
+
+    //计算数据
+    Rectangle canvas = Rectangle(width - 2*margin, height - 2*margin);
+    Scatterplot scatterplot = Scatterplot(canvas, count);
+    Delaunay delaunay = Delaunay(canvas, scatterplot.vertex_list);
+    Voronoi voronoi = Voronoi(canvas, delaunay.triangle_list);
+
+    //清空窗口
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    //绘制边框
+    glColor3d(0.0f, 0.0f, 0.0f);
+    glLineWidth(1.0f);
+    glBegin(GL_LINE_LOOP);
+    vertex2d(0.0f, 0.0f);
+    vertex2d(canvas.width, 0.0f);
+    vertex2d(canvas.width, canvas.height);
+    vertex2d(0.0f, canvas.height);
+    glEnd();
+
+    //绘制顶点
+    glColor3d(0.0f, 1.0f, 0.0f);
+    glPointSize(5.0f);
+    glBegin(GL_POINTS);
+    for (Vertex vertex : scatterplot.vertex_list) {
+      vertex2d(vertex.x, vertex.y);
+    }
+    glEnd();
+
+    //绘制三角形
+    for (Triangle triangle : delaunay.triangle_list) {
+      glColor3d(0.0f, 1.0f, 0.0f);
+      glLineWidth(1.0f);
+      glBegin(GL_LINE_LOOP);
+      vertex2d(triangle.o.x, triangle.o.y);
+      vertex2d(triangle.p.x, triangle.p.y);
+      vertex2d(triangle.q.x, triangle.q.y);
+      glEnd();
+    }
+
+    //绘制三角形外心连线
+    glColor3d(1.0f, 0.0f, 0.0f);
+    glLineWidth(1.0f);
+    glBegin(GL_LINES);
+    for (Line line : voronoi.line_list) {
+      vertex2d(line.o.x, line.o.y);
+      vertex2d(line.p.x, line.p.y);
+    }
+    glEnd();
+
+    glfwSwapBuffers(window);
+  }
 }
 
 int main() {
@@ -33,67 +97,7 @@ int main() {
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
 
-    int _width;
-    int _height;
-    glfwGetWindowSize(window, &_width, &_height);
-    
-    if (width == _width && height == _height) {
-      continue;
-    } else {
-      width = _width;
-      height = _height;
-
-      //计算数据
-      Rectangle canvas = Rectangle(width - 2*margin, height - 2*margin);
-      Scatterplot scatterplot = Scatterplot(canvas, count);
-      Delaunay delaunay = Delaunay(canvas, scatterplot.vertex_list);
-      Voronoi voronoi = Voronoi(canvas, delaunay.triangle_list);
-
-      //清空窗口
-      glClear(GL_COLOR_BUFFER_BIT);
-
-      //绘制边框
-      glColor3d(0.0f, 0.0f, 0.0f);
-      glLineWidth(1.0f);
-      glBegin(GL_LINE_LOOP);
-      vertex2d(0.0f, 0.0f);
-      vertex2d(canvas.width, 0.0f);
-      vertex2d(canvas.width, canvas.height);
-      vertex2d(0.0f, canvas.height);
-      glEnd();
-
-      //绘制顶点
-      glColor3d(0.0f, 1.0f, 0.0f);
-      glPointSize(5.0f);
-      glBegin(GL_POINTS);
-      for (Vertex vertex : scatterplot.vertex_list) {
-        vertex2d(vertex.x, vertex.y);
-      }
-      glEnd();
-
-      //绘制三角形
-      for (Triangle triangle : delaunay.triangle_list) {
-        glColor3d(0.0f, 1.0f, 0.0f);
-        glLineWidth(1.0f);
-        glBegin(GL_LINE_LOOP);
-        vertex2d(triangle.o.x, triangle.o.y);
-        vertex2d(triangle.p.x, triangle.p.y);
-        vertex2d(triangle.q.x, triangle.q.y);
-        glEnd();
-      }
-
-      //绘制三角形外心连线
-      glColor3d(1.0f, 0.0f, 0.0f);
-      glLineWidth(1.0f);
-      glBegin(GL_LINES);
-      for (Line line : voronoi.line_list) {
-        vertex2d(line.o.x, line.o.y);
-        vertex2d(line.p.x, line.p.y);
-      }
-      glEnd();
-    }
-
-    glfwSwapBuffers(window);
+    drawVoronoi(window, 50);
   }
 
   glfwTerminate();
