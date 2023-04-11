e49d04b79a6b484d13d352a5cc7dbc5700d95856
ZhengXianda <xianda.zheng@jiamiantech.com>
2023-04-11 10:07:12 +0800

    通过CGAL实现Voronoi图绘制

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,15 +1,40 @@
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/Delaunay_triangulation_adaptation_traits_2.h>
+#include <CGAL/Delaunay_triangulation_adaptation_policies_2.h>
+#include <CGAL/Voronoi_diagram_2.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <tkgeometry/tkgeometry.hpp>
 
 #include <iostream>
+#include <random>
 
 using namespace std;
 using namespace tkht;
 
+typedef CGAL::Exact_predicates_exact_constructions_kernel                     K;
+typedef CGAL::Delaunay_triangulation_2<K>                                    DT;
+typedef CGAL::Delaunay_triangulation_adaptation_traits_2<DT>                 AT;
+typedef CGAL::Delaunay_triangulation_caching_degeneracy_removal_policy_2<DT> AP;
+typedef CGAL::Voronoi_diagram_2<DT,AT,AP>                                    VD;
+
+typedef AT::Site_2                    Site_2;
+typedef AT::Point_2                   Point_2;
+typedef VD::Locate_result             Locate_result;
+typedef VD::Vertex_handle             Vertex_handle;
+typedef VD::Face_handle               Face_handle;
+typedef VD::Halfedge_handle           Halfedge_handle;
+typedef VD::Ccb_halfedge_circulator   Ccb_halfedge_circulator;
+
 int width, height = 0;
 
 int margin = 50;
+
+// 建立随机数生成器
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_real_distribution<float> dis(0.5f, 1.0f);
 
 void vertex2d(GLdouble x, GLdouble y) {
   glVertex2d((x + margin) / width * 2.0f - 1.0f,
@@ -30,8 +55,16 @@ void drawVoronoi(GLFWwindow *window, double density) {
     // 计算数据
     Rectangle canvas = Rectangle(width - 2 * margin, height - 2 * margin);
     Scatterplot scatterplot = Scatterplot(canvas, density);
-    Delaunay delaunay = Delaunay(canvas, scatterplot.vertex_list);
-    Voronoi voronoi = Voronoi(canvas, delaunay.triangle_list);
+
+    DT dt;
+    for (Point point : scatterplot.vertex_list) {
+      dt.insert(Point_2(point.x, point.y));
+    }
+
+    VD vd;
+    for (Point point : scatterplot.vertex_list) {
+      vd.insert(Point_2(point.x, point.y));
+    }
 
     // 清空窗口
     glClear(GL_COLOR_BUFFER_BIT);
@@ -45,71 +78,51 @@ void drawVoronoi(GLFWwindow *window, double density) {
     }
     glEnd();
 
-    // 绘制顶点
-    glColor3d(0.0f, 1.0f, 0.0f);
-    glPointSize(15.0f);
-    glBegin(GL_POINTS);
-    for (Point vertex : scatterplot.vertex_list) {
-      vertex2d(vertex.x, vertex.y);
-    }
-    glEnd();
-
-    // 绘制三角形
-    for (Triangle triangle : delaunay.triangle_list) {
-      glColor3d(0.0f, 1.0f, 0.0f);
+    //绘制三角形剖分图
+    for (auto it = dt.finite_faces_begin(); it != dt.finite_faces_end(); it++)
+    {
+      glColor3d(0.6f, 0.8f, 0.9f);
       glLineWidth(5.0f);
-      glBegin(GL_LINE_LOOP);
-      vertex2d(triangle.o.x, triangle.o.y);
-      vertex2d(triangle.p.x, triangle.p.y);
-      vertex2d(triangle.q.x, triangle.q.y);
+      glBegin(GL_POLYGON);
+      Point_2 o = it->vertex(0)->point();
+      Point_2 p = it->vertex(1)->point();
+      Point_2 q = it->vertex(2)->point();
+      vertex2d(CGAL::to_double(o.x()), CGAL::to_double(o.y()));
+      vertex2d(CGAL::to_double(p.x()), CGAL::to_double(p.y()));
+      vertex2d(CGAL::to_double(q.x()), CGAL::to_double(q.y()));
       glEnd();
     }
 
-    // 绘制三角形外心连线
-    glColor3d(1.0f, 0.0f, 0.0f);
-    glLineWidth(5.0f);
-    glBegin(GL_LINES);
-    for (Line line : voronoi.line_list) {
-      vertex2d(line.o.x, line.o.y);
-      vertex2d(line.p.x, line.p.y);
+    // 绘制维诺图单元格
+    for (auto it = vd.bounded_faces_begin(); it != vd.bounded_faces_end(); it++)
+    {
+      list <Point_2> buffer;
+      Halfedge_handle he = it->ccb();
+      do {
+        if (he->is_segment()) {
+          Point_2 point = he->source()->point();
+          double x = CGAL::to_double(point.x());
+          double y = CGAL::to_double(point.y());
+          if (x >= canvas.o.x && x <= canvas.p.x && y >= canvas.o.y && y <= canvas.p.y) {
+            buffer.push_back(point);
+          } else {
+            buffer.clear();
+            break;
+          }
+        }
+        he = he->next();
+      } while (he != it->ccb());
+      if (buffer.size() > 0) {
+        glColor3d(dis(gen), dis(gen), dis(gen));
+        glLineWidth(5.0f);
+        glBegin(GL_POLYGON);
+        for (Point_2 point : buffer) {
+          vertex2d(CGAL::to_double(point.x()), CGAL::to_double(point.y()));
+        }
+        glEnd();
+      }
     }
-    glEnd();
-
-    glfwSwapBuffers(window);
-  }
-}
-
-void drawPolygon(GLFWwindow *window) {
-  int _width;
-  int _height;
-  glfwGetWindowSize(window, &_width, &_height);
-
-  if (width == _width && height == _height) {
-
-  } else {
-    width = _width;
-    height = _height;
-
-    // 计算数据
-    Polygon polygon{
-        Point(0.0f, 0.0f),
-        Point(width - 2 * margin, 0.0f),
-        Point(width - 2 * margin, height - 2 * margin),
-        Point(0.0f, height - 2 * margin),
-    };
-
-    // 清空窗口
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // 绘制边框
-    glColor3d(0.0f, 0.0f, 0.0f);
-    glLineWidth(5.0f);
-    glBegin(GL_LINE_LOOP);
-    for (Point vertex : polygon.vertex_list) {
-      vertex2d(vertex.x, vertex.y);
-    }
-    glEnd();
-
+    
     glfwSwapBuffers(window);
   }
 }
@@ -133,8 +146,7 @@ int main() {
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
 
-    drawVoronoi(window, 50);
-    // drawPolygon(window);
+    drawVoronoi(window, 200);
   }
 
   glfwTerminate();
